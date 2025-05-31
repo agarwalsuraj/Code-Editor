5dde2af0b5a5c7dc6c20b5880eeb2c41663a3068
agarwalsuraj <103362715+agarwalsuraj@users.noreply.github.com>
2025-05-31 15:46:33 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -21,4 +21,5 @@ def run():
         return jsonify({'output': '', 'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from environment
+    app.run(host="0.0.0.0", port=port)
